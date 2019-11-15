710dea352475273122413053d146821b0d4b1ddd
CloudHare <jan.rimantas@gmail.com>
2019-11-15 16:15:56 +0200

    Improved threads, not done yet. Need to fix the form's bilnk. Ensure the thread are made the right way. Add "OK" button.

--- HistoryClient/Pages/LoadingBox.Designer.cs
+++ HistoryClient/Pages/LoadingBox.Designer.cs
@@ -29,15 +29,15 @@
         private void InitializeComponent()
         {
             this.text = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.text2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // text
             // 
             this.text.AutoSize = true;
+            this.text.Dock = System.Windows.Forms.DockStyle.Left;
             this.text.Font = new System.Drawing.Font("Microsoft JhengHei UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.text.Location = new System.Drawing.Point(13, 13);
+            this.text.Location = new System.Drawing.Point(0, 0);
             this.text.Name = "text";
             this.text.Padding = new System.Windows.Forms.Padding(10);
             this.text.Size = new System.Drawing.Size(51, 45);
@@ -45,27 +45,32 @@
             this.text.Text = "hi";
             this.text.Click += new System.EventHandler(this.Text_Click);
             // 
-            // pictureBox1
+            // text2
             // 
-            this.pictureBox1.Image = global::HistoryClient.Properties.Resources.Spinner_1s_200px;
-            this.pictureBox1.Location = new System.Drawing.Point(445, 13);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(51, 40);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.text2.AutoSize = true;
+            this.text2.Font = new System.Drawing.Font("Microsoft JhengHei UI", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text2.Location = new System.Drawing.Point(5, 136);
+            this.text2.Name = "text2";
+            this.text2.Size = new System.Drawing.Size(151, 18);
+            this.text2.TabIndex = 1;
+            this.text2.Text = "Loading. Please wait.";
             // 
             // LoadingBox
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(508, 65);
-            this.Controls.Add(this.pictureBox1);
+            this.AutoSize = true;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.ClientSize = new System.Drawing.Size(397, 165);
+            this.Controls.Add(this.text2);
             this.Controls.Add(this.text);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "LoadingBox";
+            this.ShowIcon = false;
             this.Text = " ";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -74,6 +79,6 @@
         #endregion
 
         private System.Windows.Forms.Label text;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label text2;
     }
 }
