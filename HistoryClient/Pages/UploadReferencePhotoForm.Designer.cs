2b89f32f619345260bf809531e80cdca66daf799
Arnas Baronas <arnasbaronas@gmail.com>
2019-11-19 00:19:27 +0200

    UI fix

--- HistoryClient/Pages/UploadReferencePhotoForm.Designer.cs
+++ HistoryClient/Pages/UploadReferencePhotoForm.Designer.cs
@@ -96,7 +96,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "UploadReferencePhoto";
-            this.Text = "We want to recognize";
+            this.Text = "We want to recognize you";
             this.ResumeLayout(false);
             this.PerformLayout();
 
